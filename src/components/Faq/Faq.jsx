bc53ecaa42c5c13b74b1ab1941e1642bcc1f63de
Md. Shahriar Hossain Alvi <shahriarhossainalvi@gmail.com>
2024-03-26 03:55:18 +0600

    created components and added their paths

---
+++ src/components/Faq/Faq.jsx
@@ -0,0 +1,41 @@
+const Faq = () => {
+    return (
+        <div>
+            <h2>FAQ</h2>
+            <div>
+                <h3>1. **How do I search for books on the website?**</h3>
+                <p>- Provide guidance on using the search feature to find books by title, author, genre, or keyword.</p>
+
+                <h3>2. **Can I purchase books directly from the website?**</h3>
+                <p>- Explain whether the website offers e-commerce functionality for purchasing books and provide information on the process.</p>
+
+                <h3>3. **Are there any discounts or promotions available?**</h3>
+                <p>- Inform users about any ongoing discounts, promotions, or special offers on books and how they can take advantage of them.</p>
+
+                <h3>4. **How can I access digital books or e-books?**</h3>
+                <p>- Describe how users can access digital books or e-books, including information on formats, compatible devices, and any necessary apps or software.</p>
+
+                <h3>5. **Do you offer recommendations or personalized reading lists?**</h3>
+                <p>- Explain whether the website provides personalized book recommendations based on user preferences and reading history.</p>
+
+                <h3>6. **Can I read book reviews or ratings on the website?**</h3>
+                <p>- Provide details on how users can access book reviews, ratings, and user-generated content to help them make informed decisions about their reading choices.</p>
+
+                <h3>7. **How do I navigate the website to find specific genres or categories?**</h3>
+                <p>- Offer guidance on navigating the websites categories, genres, and browsing options to help users discover new books of interest.</p>
+
+                <h3>8. **Are there any resources for book clubs or reading groups?**</h3>
+                <p>- Provide information on resources available for book clubs or reading groups, such as discussion guides, author Q&A sessions, or virtual events</p>.
+
+                <h3>9. **How can I contact customer support for assistance?**</h3>
+                <p>- Provide contact information for customer support, including email addresses, phone numbers, or support ticket systems, and explain how users can reach out for assistance with any queries or issues.</p>
+
+                <h3>10. **What are the websites policies on privacy, data protection, and terms of service?**</h3>
+                <p> - Provide links to the websites privacy policy, data protection measures, and terms of service to inform users about their rights and responsibilities while using the platform.</p>
+
+            </div>
+        </div>
+    );
+};
+
+export default Faq;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,49 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Root from './components/Root/Root.jsx';
+import Home from './components/Home/Home.jsx';
+import ListedBooks from './components/ListedBooks/ListedBooks.jsx';
+import PagesToRead from './components/PagesToRead/PagesToRead.jsx';
+import Contact from './components/Contact/Contact.jsx';
+import Faq from './components/Faq/Faq.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    children: [
+      {
+        path:'/',
+        element: <Home></Home>
+      },
+      {
+        path: '/listedBooks',
+        element: <ListedBooks></ListedBooks>
+      },
+      {
+        path: 'pagesToRead',
+        element: <PagesToRead></PagesToRead>
+      },
+      {
+        path: '/contact',
+        element: <Contact></Contact>
+      },
+      {
+        path: '/FAQ',
+        element: <Faq></Faq>
+      }
+    ]
+  },
+]);
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
